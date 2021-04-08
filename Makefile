b9d62d542fc30f597282d05d6735694d226760da
adgko <adgko@github.com>
2021-04-07 22:45:20 -0300

    crea procesos hijos para productores

--- Makefile
+++ Makefile
@@ -1,11 +1,22 @@
 FLAGS_GCC = -std=gnu11 -Wall -Werror -pedantic -Wextra -Wconversion
 FLAGS_CPP = --enable=performance,portability,information,unusedFunction -q
 
-make:
-	cppcheck $(FLAGS_CPP) ./
-	gcc  src/sock_server.c -o bin/server $(FLAGS_GCC)
-	gcc  src/sock_client.c -o bin/client $(FLAGS_GCC)
+all: check cliente server productores
 
 clean:
 	rm bin/*
 	#rm -r html/ latex/
+	
+check:
+	cppcheck $(FLAGS_CPP) ./
+	
+cliente:
+	gcc  src/sock_client.c -o bin/client $(FLAGS_GCC)
+	
+server:
+	gcc  src/sock_server.c -o bin/server $(FLAGS_GCC)
+	
+productores:
+	gcc src/productor1.c -o bin/prod1 $(FLAGS_GCC)
+	gcc src/productor2.c -o bin/prod2 $(FLAGS_GCC)
+	gcc src/productor3.c -o bin/prod3 $(FLAGS_GCC)
--- src/sock_server.c
+++ src/sock_server.c
@@ -1,66 +1,119 @@
-// Server side C/C++ program to demonstrate Socket programming
-#include <unistd.h>
 #include <stdio.h>
-#include <sys/socket.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include <netinet/in.h>
 #include <string.h>
-#define PORT 8080
-int main()
-{
-    int server_fd, new_socket; 
-	ssize_t valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello = "Hello from server";
-       
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-       
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-       
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address, 
-                                 sizeof(address))<0)
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-                       (socklen_t*)&addrlen))<0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-	while(1){
-    valread = recv( new_socket , buffer, 1024,0);
-	if(valread < 0){
-		perror("no se recibió la info");
-		exit(1);
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#define TAM 256
+
+#define PROD1_PATH "prod1"
+#define PROD2_PATH "prod2"
+#define PROD3_PATH "prod3"
+
+
+int main( int argc, char *argv[] ) {
+	int32_t sockfd, newsockfd, puerto, pid;
+    uint32_t clilen;
+	char buffer[TAM];
+	struct sockaddr_in serv_addr, cli_addr;
+	ssize_t n;
+	int32_t terminar = 0;
+    int32_t pid_prod1, pid_prod2, pid_prod3;
+
+	if ( argc < 2 ) {
+        	fprintf( stderr, "Uso: %s <puerto>\n", argv[0] );
+		exit( 1 );
 	}
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+
+    pid_prod1 = fork();
+  		if ( pid_prod1 == 0 ) {
+    		if( execv(PROD1_PATH, argv) == -1 ) {
+   			  perror("error en auth ");
+   			  exit(1);
+   			} 			
+   			exit(0);
+  		}
+
+  	pid_prod2 = fork();
+  		if ( pid_prod2 == 0 ) {
+    		if( execv(PROD2_PATH, argv) == -1 ) {
+   			  perror("error en file ");
+   			  exit(1);
+   			}   			
+   			exit(0);
+		}
+
+    pid_prod3 = fork();
+  		if ( pid_prod3 == 0 ) {
+    		if( execv(PROD3_PATH, argv) == -1 ) {
+   			  perror("error en file ");
+   			  exit(1);
+   			}   			
+   			exit(0);
+		}
+
+
+	sockfd = socket( AF_INET, SOCK_STREAM, 0);
+
+	memset( (char *) &serv_addr, 0, sizeof(serv_addr) );
+	puerto = atoi( argv[1] );
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons( (uint16_t)puerto );
+
+	if ( bind(sockfd, ( struct sockaddr *) &serv_addr, sizeof( serv_addr ) ) < 0 ) {
+		perror( "ligadura" );
+		exit( 1 );
 	}
-    return 0;
+
+        printf( "Proceso: %d - socket disponible: %d\n", getpid(), ntohs(serv_addr.sin_port) );
+
+	listen( sockfd, 5 );
+	clilen = sizeof( cli_addr );
+
+	while( 1 ) {
+		newsockfd = accept( sockfd, (struct sockaddr *) &cli_addr, &clilen );
+
+		pid = fork(); 
+
+		if ( pid == 0 )
+		  {  // Proceso hijo
+		    close( sockfd );
+
+		while(1) {
+		printf( "Ingrese el mensaje a transmitir: " );
+		memset( buffer, '\0', TAM );
+		fgets( buffer, TAM-1, stdin );
+
+		n = send( newsockfd, buffer, strlen(buffer),0 );
+        if(n < 0){
+            perror("fallo en enviar info");
+            exit(1);
+        }
+
+		// Verificando si se escribió: fin
+		buffer[strlen(buffer)-1] = '\0';
+		if( !strcmp( "fin", buffer ) ) {
+			terminar = 1;
+		}
+
+		memset( buffer, '\0', TAM );
+		n = recv( newsockfd, buffer, TAM,0 );
+        if(n < 0){
+            perror("fallo en recibir info");
+            exit(1);
+        }
+		printf( "Respuesta: %s\n", buffer );
+		if( terminar ) {
+			printf( "Finalizando ejecución\n" );
+			exit(0);
+		}
+		}
+
+		  }
+
+	}
+	return 0; 
+ 
 }
