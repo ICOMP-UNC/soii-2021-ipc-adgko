100ed3cd2b81cdf78eae7a84b276f160b9bef1e6
adgko <adgko@github.com>
2021-04-08 10:52:25 -0300

    productores funcionan cada cierto tiempo. Falta cola de mensajes

--- src/productor1.c
+++ src/productor1.c
@@ -2,12 +2,15 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(){
-    printf("Soy el Productor 1\n");
-    int32_t numero, t = 1;
+#define X 2
 
-    while(t){
+int32_t main(){
+    printf("Soy el Productor 1\n");
+    int32_t numero;
+
+    while(1){
         numero = rand();
         printf("El numero enviado es %d\n",numero);
+        sleep(1/X);
     }
 }
