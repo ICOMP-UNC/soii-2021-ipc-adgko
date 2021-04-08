15d1a978d599fa85bf506d5053ca91ec6dbb90cf
adgko <adgko@github.com>
2021-04-08 00:05:32 -0300

    Productor 1 hace mensajes aleatorios

--- src/productor1.c
+++ src/productor1.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main(){
+    printf("Soy el Productor 1\n");
+    int32_t numero, t = 1;
 
-    while(1){
-
+    while(t){
+        numero = rand();
+        printf("El numero enviado es %d\n",numero);
     }
-
-    return 0;
 }
